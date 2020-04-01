12b2b95d48a28d733b57f9b64a33345041d4d541
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 08:55:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(ming)
+  return"Hello, my name is #{ming}."
+end
+
+def batch_badge_creator(array)
+  array.collect{|x| "Hello, my name is #{x}."}
+  # return newarray
+end
+
+def assign_rooms(array)
+  array.collect.with_index {|a,b| "Hello, #{a}! You'll be assigned to room #{b+1}!"}
+    # array.each.do |a,b|
+    #   puts"Hello, #{a}! You'll be assigned to room #{b+1}!"
+    # end
+ end
+
+def printer(array)
+  batch_badge_creator(array).each do |x|
+    puts"#{x}"
+  end
+  assign_rooms(array).each do |y|
+    puts"#{y}"
+  end
+end
