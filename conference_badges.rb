9099660a22bc1e743509553eb4967ce7dce9d4e1
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 08:55:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,10 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-#   array.collect.with_index {|a,b| "Hello, #{a}! You'll be assigned to room #{b+1}!"}
-    array.each.do |a,b|
-      puts"Hello, #{a}! You'll be assigned to room #{b+1}!"
-    end
+  array.collect.with_index {|a,b| "Hello, #{a}! You'll be assigned to room #{b+1}!"}
+    # array.each.do |a,b|
+    #   puts"Hello, #{a}! You'll be assigned to room #{b+1}!"
+    # end
  end
 
 def printer(array)
